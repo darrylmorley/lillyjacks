bec45bdf7073a564f4d4313499a582925e7ac01c
Darryl Morley <darrylmorley.uk@gmail.com>
2021-08-09 21:41:39 +0100

    add search input & update colour for tagline

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -11,14 +11,26 @@ export default function Header() {
         />
       </Head>
       <div className="header">
-        <div></div>
+        <div className="search">
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search..."
+          />
+        </div>
         <div className="logo">
           <h1>LillyJacks</h1>
           <p>Sweet treats & gifts</p>
         </div>
         <div className="icons">
-          <AiOutlineUser style={{ marginRight: "0.5em" }} />
-          <AiOutlineShopping />
+          <AiOutlineUser
+            style={{
+              marginRight: "0.5em",
+            }}
+            className="icon"
+          />
+          <AiOutlineShopping className="icon" />
         </div>
       </div>
       <style jsx>{`
@@ -53,6 +65,13 @@ export default function Header() {
           margin-top: -1px;
           margin-left: -3.2em;
           animation: pulse 5s;
+          background: linear-gradient(orange, white);
+          background: -webkit-linear-gradient(white, orange);
+          background-clip: text;
+          -webkit-background-clip: text;
+          -moz-background-clip: text;
+          text-fill-color: transparent;
+          -webkit-text-fill-color: transparent;
         }
 
         @keyframes pulse {
@@ -71,10 +90,31 @@ export default function Header() {
           font-size: 1.6em;
         }
 
-        .icons:hover {
+        .icon {
+          background: linear-gradient(blue, red);
           background: -webkit-linear-gradient(blue, red);
           background-clip: text;
+          -webkit-background-clip: text;
+          -moz-background-clip: text;
+          text-fill-color: transparent;
           -webkit-text-fill-color: transparent;
+        }
+
+        .search {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        #search {
+          width: 18em;
+          height: 2em;
+          border: double 3px transparent;
+          border-radius: 6px;
+          background-image: linear-gradient(white, white),
+            linear-gradient(to right, red, blue);
+          background-origin: border-box;
+          background-clip: padding-box, border-box;
         }
       `}</style>
     </>
