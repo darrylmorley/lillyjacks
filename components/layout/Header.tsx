7e10910e3a97d92a056203ec6f40b5ab2dba46f0
Darryl Morley <darrylmorley.uk@gmail.com>
2021-08-09 18:26:09 +0100

    add tagline with animation

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -11,18 +11,40 @@ export default function () {
         />
       </Head>
       <div>
-        <h1 style={{ fontFamily: "Cookie" }}>LillyJacks</h1>
+        <h1>LillyJacks</h1>
+        <p>Sweet treats & gifts</p>
       </div>
       <style jsx>{`
         div {
           display: flex;
           justify-content: center;
+          align-items: center;
+          height: 10em;
         }
+
         h1 {
           font-size: 5em;
+          font-family: Cookie;
           background: -webkit-linear-gradient(blue, red);
           background-clip: text;
           -webkit-text-fill-color: transparent;
+        }
+
+        p {
+          align-self: flex-end;
+          font-family: Cookie;
+          font-size: 1.8em;
+          margin-left: -3.5em;
+          animation: pulse 10s;
+        }
+
+        @keyframes pulse {
+          0% {
+            color: #fff;
+          }
+          100% {
+            color: #000;
+          }
         }
       `}</style>
     </>
